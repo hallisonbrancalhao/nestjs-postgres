d636125e6ad2a3c4b353da251a9128c37d78e224
Hallison Brancalhão <hallison.o.b@gmail.com>
2023-10-07 00:08:36 -0300

    style: delete old path files

--- src/pedido/dto/update-pedido.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsEnum } from 'class-validator';
-import { StatusPedido } from '../../produto/enum/status-pedido.enum';
-
-export class UpdatePedidoDTO {
-  @IsEnum(StatusPedido)
-  status: StatusPedido;
-}
